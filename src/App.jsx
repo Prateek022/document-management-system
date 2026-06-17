ca1cc276b8a9167af626c4ebc870a4924d43379c
Prateek <prateek.ps988@gmail.com>
2026-06-18 00:07:42 +0530

    Integrate document upload API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Upload from "./pages/Upload";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/search" element={<Search />} />
         <Route path="/" element={<Login />} />
         <Route path="/upload" element={<Upload />} />
       </Routes>
