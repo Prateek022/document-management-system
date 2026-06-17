44e4d534423174a43fcfa81ea7f8e68e569f1c45
Prateek <prateek.ps988@gmail.com>
2026-06-17 22:36:20 +0530

    Add upload form date and category fields

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -2,6 +2,18 @@ function Upload() {
   return (
     <div>
       <h1>Upload Documents</h1>
+
+      <input type="date" />
+
+      <br />
+      <br />
+
+      <select>
+        <option>Select Category</option>
+        <option>Invoice</option>
+        <option>Contract</option>
+        <option>Report</option>
+      </select>
     </div>
   );
 }
