415e34f91d5e9d1618cd6c91afaa8356d59d6efd
Prateek <prateek.ps988@gmail.com>
2026-06-18 02:08:51 +0530

    Improve admin page user interface

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
 
+
 function Admin() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     return (
-    <div>
-        <h1>Create User</h1>
+        <div className="container mt-4">
+            <div className="card p-4 shadow">
+                <h1 className="mb-4">Create User</h1>
 
-        <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        />
+                <input
+                    className="form-control mb-3"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
 
-        <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        />
+                <input
+                    className="form-control mb-3"
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-        <button>Create User</button>
-    </div>
+                <button className="btn btn-primary">
+                    Create User
+                </button>
+            </div>
+        </div>
     );
 }
 
