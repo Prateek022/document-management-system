4fdd2e66ead7cd9bed48878cf2fa886545b9201d
Prateek <prateek.ps988@gmail.com>
2026-06-17 22:59:49 +0530

    Add category, subcategory and remarks fields

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,8 +1,24 @@
 import { useState } from "react";
 function Upload() {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [tags, setTags] = useState("");
+    const [category, setCategory] = useState("");
+    const [subCategory, setSubCategory] = useState("");
+    const [remarks, setRemarks] = useState("");
     return (
     <div>
+    <input
+            type="text"
+            placeholder="Enter tags separated by commas"
+            value={tags}
+            onChange={(e) => setTags(e.target.value)}
+/>  
+    <p>Tags: {tags}</p>
+    <textarea
+    placeholder="Enter Remarks"
+    value={remarks}
+    onChange={(e) => setRemarks(e.target.value)}
+/>
     <h1>Upload Documents</h1>
 
     <input type="date" />
@@ -10,12 +26,37 @@ function Upload() {
     <br />
     <br />
 
-    <select>
-        <option>Select Category</option>
-        <option>Invoice</option>
-        <option>Contract</option>
-        <option>Report</option>
-    </select>
+    <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+>
+        <option value="">Select Category</option>
+        <option value="Personal">Personal</option>
+        <option value="Professional">Professional</option>
+</select>
+<select
+    value={subCategory}
+    onChange={(e) => setSubCategory(e.target.value)}
+>
+    <option value="">Select Option</option>
+
+    {category === "Personal" && (
+    <>
+        <option value="John">John</option>
+        <option value="Tom">Tom</option>
+        <option value="Emily">Emily</option>
+    </>
+)}
+
+{category === "Professional" && (
+    <>
+        <option value="Accounts">Accounts</option>
+        <option value="HR">HR</option>
+        <option value="IT">IT</option>
+        <option value="Finance">Finance</option>
+    </>
+)}
+</select>  
     <br />
     <br />
 
