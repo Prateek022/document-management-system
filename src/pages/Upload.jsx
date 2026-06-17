d8c55654a755a3acd65e1ebc1ba5ccf54603fe38
Prateek <prateek.ps988@gmail.com>
2026-06-17 22:46:03 +0530

    Add file selection state management

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
 function Upload() {
-  return (
+    const [selectedFile, setSelectedFile] = useState(null);
+    return (
     <div>
-      <h1>Upload Documents</h1>
+    <h1>Upload Documents</h1>
 
-      <input type="date" />
+    <input type="date" />
 
-      <br />
-      <br />
+    <br />
+    <br />
 
-      <select>
+    <select>
         <option>Select Category</option>
         <option>Invoice</option>
         <option>Contract</option>
         <option>Report</option>
-      </select>
+    </select>
+    <br />
+    <br />
+
+        <input
+            type="file"
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+        />
+        <p>{selectedFile?.name}</p>
     </div>
-  );
+);
 }
 
 export default Upload;
